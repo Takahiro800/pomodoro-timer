f67fab0e9e3d0f376ecc07e273227484cc08c6fe
Takahiro800 <52635809+Takahiro800@users.noreply.github.com>
2020-04-15 18:39:53 +0900

    10秒タイマーの出力

--- timer.js
+++ timer.js
@@ -2,12 +2,12 @@ let TIME = 10;
 const ONE_MSEC = 1000;
 
 let TIMER = setInterval(
-  function() {
-    console.log( TIME );
+  function() { Count_Down() }, ONE_MSEC);
+
+function Count_Down() {
+  time_text.innerHTML = TIME;
     TIME--;
     if ( TIME < 0 ) {
-      clearInterval( TIMER )
-    }
-  },
-  ONE_MSEC
-);
+      clearInterval( TIMER );
+}
+}
