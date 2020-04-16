888a9f98650bbd3b3c1a2dc63b06cae909352f30
Takahiro800 <52635809+Takahiro800@users.noreply.github.com>
2020-04-16 12:16:15 +0900

    調整

--- timer.js
+++ timer.js
@@ -4,23 +4,24 @@ const ONE_MSEC = 1000;
 let TIMER = setInterval(
   function() { Count_Down() }, ONE_MSEC);
 
-function Count_Down() {
-  time_text.innerHTML = TIME;
-    TIME--;
-    if ( TIME < 0 ) {
-      clearInterval( TIMER );
-}
-}
 
-let is_odd_click = fales;
+// ボタン表示
+  let Button_Status_Setter = (is_idle) => {
+    const text = is_idle ? 'START' : 'CLEAR';
+    const back_ground_color = is_idle ? 'red' : 'green';
+    const border_color = is_idle ? '#ff7070' : '#308030';
+    button_text.innerHTML = text;
 
-function Click_Button() {
-  is_odd_click = !is_odd_click;
-
-  if (is_odd_click) {
-
+    const button = document.getElementById("button_circle");
+    button.style.backgroundColor = back_ground_color;
+    button.style.borderBottomColor = border_color;
   }
-  else {
 
+
+//残り時間の表示
+  let Count_Show = (time) => {
+    let minute = parseInt( time / 60 );
+    let second = parseInt( time % 60 );
+    minute_shower.innerHTML = minute;
+    second_shower.innerHTML = second;
   }
-};
